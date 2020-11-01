43a8ca8a8f304ea3efa5c0352dbabe92801eb294
laisiri <thonyang08@gmail.com>
2020-11-01 08:47:09 +0700

    initial react project for test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+/*
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Test React In FreeCodeCamp</h1>
     </div>
   );
+}*/
+function Curriculum(props) {
+  return (
+    <div>
+      <h3>Curriculum</h3>
+      <p>{props.curri}</p>
+    </div>
+  
+  );
+}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Test React In FreeCodeCamp</h1>
+        <Curriculum curri = {"Compose React Component"} />
+      </div>
+    );
+
+  }
 }
 
 export default App;
